347cd6ce81f2cee9d801c5a9d762d41d04bbf689
Guilherme Samuel <45369782+guilhermesam@users.noreply.github.com>
2020-06-02 23:33:16 -0300

    adição de métodos
    inclusão dos métodos factorial_sum e fast_prime_numbers

--- training/python/numbers.py
+++ training/python/numbers.py
@@ -1,10 +1,33 @@
-# Create a list of numbers in descending order, based on a positive integer passed as a parameter
+# Creates a list of numbers in descending order, based on a positive integer passed as a parameter
 def descending_order(number):
     if number >= 0:
         return (int(''.join(sorted(str(number), reverse=True))))
     else:
-        raise ValueError('Value < 0 isn't acceptable')
-        
+        raise ValueError('Value < 0 is not acceptable')
+
+# Reed two numbers "first" and "second" and return the sum of their factorials
+def factorial_sum(first, second):
+    sum_first = 1
+    sum_second = 1
+    for i in range(1 ,first+1):
+        sum_first *= i
+    for i in range(1 ,second+1):
+        sum_second *= i
+    return sum_first + sum_second
+
+# Read a number and returns 'prime' or 'not prime'
+def fast_prime_number(number):
+    if number > 0:
+        if number == 1:
+            return 'Prime'
+        for i in range(2, number):
+            if number % i == 0:
+                return 'Not Prime'
+            else:
+                return 'Prime'
+    else:
+        raise ValueError('Value <= 0 is not acceptable')
+                         
 # Returns a list which contains a fibonnacci sequence, being list[-1] < param
 def fibonnacci(num_param):
     if num >= 0:
@@ -15,7 +38,7 @@ def fibonnacci(num_param):
             a, b  = b, a + b
         return fibonnacci
     else:
-        raise ValueError('Value < 0 isn't acceptable')
+        raise ValueError('Value < 0 is not acceptable')
         
 # Print a sequence of numbers, at the format:
 # 1
@@ -28,4 +51,4 @@ def triangle_quest(length):
         for i in range(1, length):
         print(str(i) * i)
     else:
-        raise ValueError('Value < 0 isn't acceptable')
+        raise ValueError('Value < 0 is not acceptable')
